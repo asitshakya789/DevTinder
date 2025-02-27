195c27e9c8fe08aeb9e8abb1ccd4900f0d011613
asitshakya678 <asitshakya789@gmail.com>
2025-02-27 21:28:53 +0530

    added  server  filet

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+// Home route
+app.get("/", (req, res) => {
+    res.send("Hello World namaste from the deshbord");
+    res.send("Hello World");
+    
+});
+
+// About route
+app.get("/about", (req, res) => {
+    res.send("About Us");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
